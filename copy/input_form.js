40c0d35a2c4f701c0e50e38b9cea944ebdadd45e
kumarrajusahu <145864310+kumarrajusahu@users.noreply.github.com>
2024-05-18 14:04:32 +0530

    Add files via upload

---
+++ copy/input_form.js
@@ -0,0 +1,5 @@
+document.getElementById("myForm").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevent the form from submitting
+    // Add your form submission logic here
+    alert("Form submitted!"); // For demonstration purposes
+});
